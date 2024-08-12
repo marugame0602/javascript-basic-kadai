cbef666c5f877b77c145b3ce9b35f9b14d774519
marugame0602 <marugame0602@icloud.com>
2024-08-12 22:33:13 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,6 +1,9 @@
-let num = 5;
+let num = 24;
 
-if (num % 3 === 0){
+if (num % 15=== 0){
+  console.log('3と5の倍数です');}
+
+else if (num % 3 === 0){
   console.log('3の倍数です');
 } 
 
@@ -8,6 +11,4 @@ else if (num % 5 ===0){
   console.log ('5の倍数です') ;
 } 
 
-else if (num % 15=== 0){
-console.log('3と5の倍数です');}
 else {console.log(num)};
